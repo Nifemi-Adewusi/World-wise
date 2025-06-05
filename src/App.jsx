79ac3e1dc41e9062b8c3daf6b88fa0d6cfddc565
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-05 17:56:20 +0100

    Update Context

--- src/App.jsx
+++ src/App.jsx
@@ -10,72 +10,58 @@ import CityList from "./Components/CityList";
 import CountryList from "./Components/CountryList";
 import City from "./Components/City";
 import Form from "./Components/form";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 import { Context } from "./contexts/Context";
 /* eslint-disable react/prop-types */
 
 // const BASE_URL = "http://localhost:8000/cities";
-// function App() {
-//   const [cities, setCities] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
+function App() {
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
 
-//   const deleteCity = function (id) {
-//     const updatedCity = cities.filter((city) => city.id !== id);
-//     setCities(updatedCity);
-//   };
-//   useEffect(function () {
-//     try {
-//       const loadData = async function () {
-//         setIsLoading(true);
-//         const response = await fetch(BASE_URL);
-//         setIsLoading(false);
-//         if (!response.ok) {
-//           throw new Error("Failed to fetch cities");
-//         }
-//         const data = await response.json();
-//         setCities(data);
-//       };
-//       loadData();
-//     } catch (err) {
-//       alert(`${err.message} error occurred`);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   }, []);
+  //   const deleteCity = function (id) {
+  //     const updatedCity = cities.filter((city) => city.id !== id);
+  //     setCities(updatedCity);
+  //   };
+  //   useEffect(function () {
+  //     try {
+  //       const loadData = async function () {
+  //         setIsLoading(true);
+  //         const response = await fetch(BASE_URL);
+  //         setIsLoading(false);
+  //         if (!response.ok) {
+  //           throw new Error("Failed to fetch cities");
+  //         }
+  //         const data = await response.json();
+  //         setCities(data);
+  //       };
+  //       loadData();
+  //     } catch (err) {
+  //       alert(`${err.message} error occurred`);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }, []);
   return (
-    <BrowserRouter>
-      <Context>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="login" element={<Login />} />
-        <Route path="layout" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route
-            path="cities"
-            element={
-              <CityList
-                deleteCity={deleteCity}
-                isLoading={isLoading}
-                cities={cities}
-              />
-            }
-          />
-          <Route path="cities/:id" element={<City />} />
-          <Route
-            path="countries"
-            element={<CountryList cities={cities} isLoading={isLoading} />}
-          />
-          <Route path="form" element={<Form />} />
-        </Route>
+    <Context>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="login" element={<Login />} />
+          <Route path="layout" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
         </Routes>
-      </Context>
-        
-    </BrowserRouter>
+      </BrowserRouter>
+    </Context>
   );
 }
 
