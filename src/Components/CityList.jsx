928045ea46639afe1612e67446ac77b772655efd
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-05 18:09:25 +0100

    Update CityList With Context Hook

--- src/Components/CityList.jsx
+++ src/Components/CityList.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable react/prop-types */
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
-import CityItem from './CityItem';
-import Message from './Message'
+import CityItem from "./CityItem";
+import Message from "./Message";
+// import { useContext } from "react";
+import { useCities } from "../contexts/Context";
 
-function CityList({ cities, isLoading, deleteCity }) {
+function CityList() {
+  const { cities, isLoading, deleteCity } = useCities();
   if (isLoading) return <Spinner />;
-  if(!cities.length){return <Message message = "No Cities Yet"/>}
-    return <ul className={styles.cityList}>
-      {cities.map(city=> <CityItem deleteCity = {deleteCity} key = {city.id} city = {city}/>)}
-  </ul>;
+  if (!cities.length) {
+    return <Message message="No Cities Yet" />;
+  }
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
