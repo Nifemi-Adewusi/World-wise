8635b60940aaf0f2325a7aa442c8202b30230f1f
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-09 20:43:33 +0100

    Update City

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -2,7 +2,8 @@
 /* eslint-disable no-unused-vars */
 import styles from "./City.module.css";
 import {useParams, useSearchParams} from "react-router-dom";
-import {useCities} from '../contexts/Context'
+import {useCities} from '../contexts/Context';
+import {useEffect} from "react";
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -15,6 +16,7 @@ function City() {
   const X = useParams();
   const {id} = X;
   const {getCity, currentCity} =  useCities();
+  console.log(currentCity)
   // const [searchParam, setSearchParam] = useSearchParams();
   // const lat = searchParam.get("lat");
   // const lng = searchParam.get("lng");
@@ -27,50 +29,54 @@ function City() {
   //   notes: "My favorite city so far!",
   // };
 
+  // useEffect(function(){
+  //   getCity(id)
+  // }, [id]);
+
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
    
-    // <div className={styles.city}>
-    //   <div className={styles.row}>
-    //     <h6>City name</h6>
-    //     <h3>
-    //       <span>{emoji}</span> {cityName}
-    //     </h3>
-    //   </div>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{emoji}</span> {cityName}
+        </h3>
+      </div>
 
-    //   <div className={styles.row}>
-    //     <h6>You went to {cityName} on</h6>
-    //     <p>{formatDate(date || null)}</p>
-    //   </div>
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
 
-    //   {notes && (
-    //     <div className={styles.row}>
-    //       <h6>Your notes</h6>
-    //       <p>{notes}</p>
-    //     </div>
-    //   )}
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
 
-    //   <div className={styles.row}>
-    //     <h6>Learn more</h6>
-    //     <a
-    //       href={`https://en.wikipedia.org/wiki/${cityName}`}
-    //       target="_blank"
-    //       rel="noreferrer"
-    //     >
-    //       Check out {cityName} on Wikipedia &rarr;
-    //     </a>
-    //   </div>
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
 
-    //   <div>
-    //     {/* <ButtonBack /> */}
-    //   </div>
-    // </div>
-    <>
+      <div>
+        {/* <ButtonBack /> */}
+      </div>
+    </div>
+    // <>
     
-    <h1>City {id}</h1>
-    <h2>{lat}:{lng}</h2>
-    </>
+    // <h1>City {id}</h1>
+    // <h2>{lat}:{lng}</h2>
+    // </>
 
   );
 }
