cff3579c2a07c1b240adbe37e4b010c3e1780328
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-10 15:52:28 +0100

    Add Map Feature

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,15 +1,36 @@
 /* eslint-disable no-unused-vars */
-import styles from './Map.module.css'
-import {useSearchParams, useNavigate} from "react-router-dom"
+import styles from "./Map.module.css";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../contexts/Context";
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const [position, setPosition] = useState([40, 0]);
+  const { cities } = useCities();
   return (
-    <div className = {styles.mapContainer} onClick = {()=> navigate("form")}>
-      <h2>Map Component</h2>
-      <h1>Position:{lat}:{lng}</h1>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        className={styles.map}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker key={city.id} position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
