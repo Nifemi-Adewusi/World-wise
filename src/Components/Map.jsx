113bd6416ac1368d2611263211bbf68ed84b7870
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-11 15:08:13 +0100

    Update Map To Respond To Centering Based On Clicks

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable no-unused-vars */
 import styles from "./Map.module.css";
 import { useSearchParams, useNavigate } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup,useMap } from "react-leaflet";
 import { useState } from "react";
 import { useCities } from "../contexts/Context";
 function Map() {
   // const [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
   // const mapLat = searchParams.get("lat");
   // const mapLng = searchParams.get("lng");
   // console.log(mapLat, mapLng);
@@ -41,8 +40,23 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position = {currentPosition}/>
       </MapContainer>
     </div>
   );
+}
+/* eslint-disable react/prop-types */
+function ChangeCenter({position}){
+  const map = useMap();
+    map.setView(position);
+    return null;
+}
+
+function DetectClick(){
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click:(e)=>navigate("form")
+  })
 }
 export default Map;
