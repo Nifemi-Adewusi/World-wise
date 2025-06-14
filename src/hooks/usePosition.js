765827314d9dfd871503e0d1323e00a67efaa236
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-14 07:09:55 +0100

    Change exports to an array

--- src/hooks/usePosition.js
+++ src/hooks/usePosition.js
@@ -3,7 +3,7 @@ function useUrlPosition() {
   const [searchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  return { lat, lng };
+  return [lat, lng];
 }
 
 export { useUrlPosition };
