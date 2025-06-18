3d771feecc7b08356baa2a0af2347f0ff64cfb0b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-19 00:47:20 +0100

    Implementing The useReducer hook

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -1,29 +1,60 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect, createContext, useContext } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  useReducer,
+} from "react";
 
 const CitiesContext = createContext();
 const BASE_URL = "http://localhost:8000/cities";
+
+const initialState = {
+  cities: [],
+  isLoading: false,
+  currentCity: {},
+  currentPosition: [40, 0],
+  currentCountry: "",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "cities/loaded":
+      return null;
+    case "cities/created":
+      return null;
+    case "cities/deleted":
+      return null;
+  }
+}
 function Context({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
-  const [currentPosition, setCurrentPosition] = useState([40, 0]);
-  const [currentCountry, setCurrentCountry] = useState("");
+  // const [cities, setCities] = useState([]);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [currentCity, setCurrentCity] = useState({});
+  // const [currentPosition, setCurrentPosition] = useState([40, 0]);
+  // const [currentCountry, setCurrentCountry] = useState("");
+
+  // const {  } =
+  //   useReducer(initialState, reducer);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { cities, isLoading, currentCity, currentPosition, currentCountry } =
+    state;
+
   const deleteCity = async function (id) {
-    try{
+    try {
       setIsLoading(true);
       const res = await fetch(`${BASE_URL}/${id}`, {
         method: "DELETE",
-      })
-      if(!res.ok) throw new Error("Failed to delete city");
+      });
+      if (!res.ok) throw new Error("Failed to delete city");
       const updatedCity = cities.filter((city) => city.id !== id);
       setCities(updatedCity);
-    } catch(err){
+    } catch (err) {
       alert(`${err.message} error occured`);
-    } finally{
+    } finally {
       setIsLoading(false);
     }
-  
   };
   useEffect(function () {
     try {
@@ -70,7 +101,7 @@ function Context({ children }) {
   //       },
   //     });
   //     const data = await res.json();
-     
+
   //     setCities((cities=> [...cities, data]))
   //   } catch (err) {
   //     alert(`${err.message} error occurred`);
@@ -89,9 +120,9 @@ function Context({ children }) {
           "Content-Type": "application/json",
         },
       });
-  
+
       if (!res.ok) throw new Error("Failed to create city");
-  
+
       const data = await res.json();
       setCities((cities) => [...cities, data]);
       setCurrentCity(data);
@@ -101,7 +132,6 @@ function Context({ children }) {
       setIsLoading(false);
     }
   }
-  
 
   return (
     <CitiesContext.Provider
@@ -117,7 +147,7 @@ function Context({ children }) {
         setCurrentPosition,
         currentCountry,
         setCurrentCountry,
-        createCity
+        createCity,
       }}
     >
       {children}
