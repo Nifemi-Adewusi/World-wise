56cf3b41288b576f488f4639151a79c7193f4f8b
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-23 18:05:35 +0100

    Add A ProtectedRoute

---
+++ src/Pages/ProtectedRoute.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth();
+  useEffect(
+    function () {
+      if (!isAuthenticated) {
+        navigate("/");
+      }
+    },
+    [isAuthenticated, navigate]
+  );
+  return children;
+}
+
+export default ProtectedRoute;
