663151049322bfc1ae192f7f22af93705b555651
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-23 15:56:42 +0100

    Update Pages To Reflect New Changes

--- src/Pages/AppLayout.jsx
+++ src/Pages/AppLayout.jsx
@@ -1,13 +1,15 @@
 // import AppNav from "../Components/AppNav";
 import SideBar from "../Components/Sidebar";
-import Map from '../Components/Map'
+import Map from "../Components/Map";
 // import styles from "./AppLayout.module.css";
-import styles from './AppLayout.module.css'
+import styles from "./AppLayout.module.css";
+import User from "../Components/User";
 function AppLayout() {
   return (
-    <div className = {styles.app}>
+    <div className={styles.app}>
       <SideBar />
-      <Map/>
+      <User />
+      <Map />
     </div>
   );
 }
