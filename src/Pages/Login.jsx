28c831cafeb275ddba3cd0ddc0c351691d066ff3
Nifemi Adewusi <nifemiadewusi@gmail.com>
2025-06-23 15:37:16 +0100

    Update The Login Page To Use Login Data

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,19 @@
 import styles from "./Login.module.css";
 import { useState } from "react";
 import PageNav from "../Components/ProjectNav";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
 
+  if (isAuthenticated) {
+    navigate("/layout");
+  }
   return (
     <main className={styles.login}>
       <PageNav />
@@ -32,7 +39,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <button onClick={() => login(email, password)}>Login</button>
         </div>
       </form>
     </main>
